2be760c5e06d88352e498d95caf45f9f9f4ca587
Nikki Dionisio <nicoellecaitlyne.dionisio@benilde.edu.ph>
2026-06-20 13:08:46 +0800

    Task 2: Fill Minigame (Implemented)
    Moved Fill Minigame Task into Main Scene, Other Scene will be for creating and backups

---
+++ OceanX_GameJam/Assets/Dialogue System/DialogueManager.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _dialoguePanel;
+    [SerializeField] private DialogueUI _dialogueUI;
+
+    [SerializeField]
+    private List<DialogueLine> _dialogueLines =
+        new List<DialogueLine>();
+
+    private int _currentLine = 0;
+
+    private void Start()
+    {
+        _dialoguePanel.SetActive(true);
+        ShowCurrentLine();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_dialogueUI.IsTyping())
+            {
+                _dialogueUI.FinishTyping();
+            }
+            else
+            {
+                NextLine();
+            }
+        }
+    }
+
+    private void ShowCurrentLine()
+    {
+        DialogueLine line =
+            _dialogueLines[_currentLine];
+
+        _dialogueUI.SetLine(
+            line.Speaker,
+            line.Text
+        );
+    }
+
+    private void NextLine()
+    {
+        _currentLine++;
+
+        if (_currentLine >= _dialogueLines.Count)
+        {
+            EndDialogue();
+            return;
+        }
+
+        ShowCurrentLine();
+    }
+
+    private void EndDialogue()
+    {
+        _dialoguePanel.SetActive(false);
+
+        Debug.Log("Dialogue Finished!");
+    }
+}
---
+++ OceanX_GameJam/Assets/Dialogue System/DialogueUI.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _speakerText;
+    [SerializeField] private TMP_Text _dialogueText;
+
+    [SerializeField] private float _typingSpeed = 0.03f;
+
+    private Coroutine _typingCoroutine;
+
+    private string _fullText;
+    private bool _isTyping;
+
+    public void SetLine(string speaker, string dialogue)
+    {
+        _speakerText.text = speaker;
+
+        if (_typingCoroutine != null)
+        {
+            StopCoroutine(_typingCoroutine);
+        }
+
+        _typingCoroutine = StartCoroutine(CO_TypeText(dialogue));
+    }
+
+    private IEnumerator CO_TypeText(string dialogue)
+    {
+        _isTyping = true;
+        _fullText = dialogue;
+
+        _dialogueText.text = "";
+
+        foreach (char letter in dialogue)
+        {
+            _dialogueText.text += letter;
+
+            yield return new WaitForSeconds(_typingSpeed);
+        }
+
+        _isTyping = false;
+        _typingCoroutine = null;
+    }
+
+    public bool IsTyping()
+    {
+        return _isTyping;
+    }
+
+    public void FinishTyping()
+    {
+        if (!_isTyping)
+            return;
+
+        StopCoroutine(_typingCoroutine);
+
+        _dialogueText.text = _fullText;
+
+        _isTyping = false;
+    }
+}
